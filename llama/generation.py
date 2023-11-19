8ca7658b572520ba5b4fcfd78e19a714a9ee9305
Suraj Bidnur <bidnur.suraj@gmail.com>
2023-11-18 19:51:27 -0800

    removing commented not needed code

--- llama/generation.py
+++ llama/generation.py
@@ -10,11 +10,6 @@ from typing import List, Literal, Optional, Tuple, TypedDict
 
 import torch
 import torch.nn.functional as F
-#from fairscale.nn.model_parallel.initialize import (
-#    get_model_parallel_rank,
-#    initialize_model_parallel,
-#    model_parallel_is_initialized,
-#)
 
 from llama.model import ModelArgs, Transformer
 from llama.tokenizer import Tokenizer
@@ -81,29 +76,9 @@ class Llama:
             and loads the pre-trained model and tokenizer.
 
         """
-#        if not torch.distributed.is_initialized():
-#            torch.distributed.init_process_group("nccl")
-#        if not model_parallel_is_initialized():
-#            if model_parallel_size is None:
-#                model_parallel_size = int(os.environ.get("WORLD_SIZE", 1))
-#            initialize_model_parallel(model_parallel_size)
-#
-#        local_rank = int(os.environ.get("LOCAL_RANK", 0))
-#        torch.cuda.set_device(local_rank)
-#
-#        # seed must be the same in all processes
-#        torch.manual_seed(seed)
-#
-#        if local_rank > 0:
-#            sys.stdout = open(os.devnull, "w")
-
         start_time = time.time()
         checkpoints = sorted(Path(ckpt_dir).glob("*.pth"))
         assert len(checkpoints) > 0, f"no checkpoint files found in {ckpt_dir}"
-#        assert model_parallel_size == len(
-#            checkpoints
-#        ), f"Loading a checkpoint for MP={len(checkpoints)} but world size is {model_parallel_size}"
-#        ckpt_path = checkpoints[get_model_parallel_rank()]
         ckpt_path = checkpoints[0]
         checkpoint = torch.load(ckpt_path, map_location="cpu")
         with open(Path(ckpt_dir) / "params.json", "r") as f:
@@ -112,6 +87,7 @@ class Llama:
         model_args: ModelArgs = ModelArgs(
             max_seq_len=max_seq_len,
             max_batch_size=max_batch_size,
+            #n_layers = 1, n_heads = 1,
             **params,
         )
         tokenizer = Tokenizer(model_path=tokenizer_path)
@@ -185,7 +161,6 @@ class Llama:
             )
 
         for cur_pos in range(min_prompt_len, total_len):
-            #logits = self.model.forward(tokens[:, prev_pos:cur_pos], prev_pos)
             logits = self.model.forward(tokens[:, :cur_pos], prev_pos)
             if temperature > 0:
                 probs = torch.softmax(logits[:, -1] / temperature, dim=-1)
