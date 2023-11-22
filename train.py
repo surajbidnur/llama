b518115ab2099a47a4215dd701a0a4ae086f6241
Suraj Bidnur <bidnur.suraj@gmail.com>
2023-11-22 14:21:42 -0800

    training partly working, gradients not updating

--- train.py
+++ train.py
@@ -14,10 +14,10 @@ DATASET_PATH = 'alpaca_dataset/alpaca_data.json'
 TOKENIZER_PATH = '/project/saifhash_1190/llama2-7b/tokenizer.model'
 CKPT_DIR = '/project/saifhash_1190/llama2-7b'
 MAX_SEQ_LEN = 512
-MAX_BATCH_SIZE = 1
+MAX_BATCH_SIZE = 2
 EPOCHS = 2
 PROMPTS = 20
-IGNORE_INDEX = -1
+IGNORE_INDEX = -100
 
 def load_dataset(path, n=200):
     """loads json dataset from filepath. If n=-1, full dataset is read, else only n items are read"""
@@ -54,7 +54,7 @@ def formatted_prompt(prompt):
         return prompt_with_input(prompt)
 
 
-def pad_sequence(sequence, padding_token, max_seq_len=512, position='left'):
+def pad_sequence(sequence, padding_token, max_seq_len=MAX_SEQ_LEN, position='left'):
 
     curr_len = len(sequence)
 
@@ -85,14 +85,10 @@ def tokenized_dict(tokens):
             labels_len=labels_len,
             )
 
-def tokenize_data(examples, tokenizer, pad_token=0, max_len=512, pad_position='right'):
-    #tokenized = [torch.tensor(tokenizer.encode(pad_sequence(s, pad_token, max_len, pad_position), bos=False, eos=False)) for s in examples]
-    tokenized = [tokenizer.encode(s, bos=False, eos=False) for s in examples]
-    pad_tokenized = [torch.tensor(pad_sequence(s, pad_token, max_len, pad_position)) for s in tokenized]
-    #print(tokenized[:2])
-    #sys.exit()
-    input_ids = labels = [tokens for tokens in pad_tokenized]
-    input_ids_len = labels_len = [tokens.ne(0).sum().item() for tokens in pad_tokenized]
+def tokenize_data(examples, tokenizer):
+    tokenized = [torch.tensor(tokenizer.encode(s, bos=False, eos=False)) for s in examples]
+    input_ids = labels = [tokens for tokens in tokenized]
+    input_ids_len = labels_len = [tokens.ne(tokenizer.pad_id).sum().item() for tokens in tokenized]
 
     return dict(
             input_ids=input_ids,
@@ -117,23 +113,16 @@ class SupervisedDataset(Dataset):
 
     def __init__(self, json_path, tokenizer, n=200):
         super(SupervisedDataset, self).__init__()
-    #def __init__(self, prompt_ds):
-        #self.prompt_ds = prompt_ds
+
         with open(json_path, "r") as f:
             prompt_ds = json.load(f)
 
         if n < len(prompt_ds):
             prompt_ds = prompt_ds[:n]
 
-        #prompt_input, prompt_no_input = prompt_dict["prompt_input"], prompt_dict["prompt_no_input"]
+        sources = [formatted_prompt(example) for example in prompt_ds]
 
-        #sources = [
-        #        prompt_no_input.format_map(example) if example["input"] == "" else prompt_input.format_map(example)
-        #        for example in prompt_ds
-        #        ]
-        sources = [prompt_no_input(example) if example["input"] == "" else prompt_with_input(example) for example in prompt_ds]
-
-        targets = [f"{example['output']}{tokenizer.eos_id}" for example in prompt_ds]
+        targets = [example['output'] + str(tokenizer.eos_id) for example in prompt_ds]
 
         data_dict = preprocess(sources, targets, tokenizer)
 
@@ -144,12 +133,6 @@ class SupervisedDataset(Dataset):
         return len(self.input_ids)
 
     def __getitem__(self, i):
-        #input_ids = self.prompt_ds[idx]['input_ids']
-        #labels = self.prompt_ds[idx]['labels']
-        #input_ids = self.prompt_ds[idx][:-1]
-        #labels = self.prompt_ds[idx][1:]
-        #sample = {'inputs': input_ids, 'target': labels}
-        #sample = {'inputs': torch.tensor(input_ids), 'target': torch.tensor(labels)}
         return dict(input_ids=self.input_ids[i], labels=self.labels[i])
 
         #return sample
@@ -163,13 +146,13 @@ class DataCollatorForSupervisedDataset(object):
     def __call__(self, instances):
             input_ids, labels = tuple([instance[key] for instance in instances] for key in ("input_ids", "labels"))
             input_ids = torch.nn.utils.rnn.pad_sequence(
-                        input_ids, batch_first=True, padding_value=0
+                        input_ids, batch_first=True, padding_value=-1
                         )
             labels = torch.nn.utils.rnn.pad_sequence(labels, batch_first=True, padding_value=IGNORE_INDEX)
             return dict(
                 input_ids=input_ids,
                 labels=labels,
-                attention_mask=input_ids.ne(0),
+                attention_mask=input_ids.ne(-1),
                 )
 
 def make_supervised_data_module(tokenizer, data_path):
@@ -180,6 +163,8 @@ def make_supervised_data_module(tokenizer, data_path):
 
 def train(ds_path, tokenizer_path, ckpt_path):
 
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
     llama_class = Llama.build(
             ckpt_dir=ckpt_path,
             tokenizer_path=tokenizer_path,
@@ -189,22 +174,22 @@ def train(ds_path, tokenizer_path, ckpt_path):
 
     model = llama_class.model
     Tokenizer = llama_class.tokenizer
+    print(model)
 
     #freeze most of the model param to save memory for debugging purpose
-    for name, param in model.named_parameters():
-        if "lm_head" not in name:
-            param.requires_grad = False
+    #for name, param in model.named_parameters():
+    #    if "lm_head" not in name:
+    #        param.requires_grad = False
 
     
     data_module = make_supervised_data_module(Tokenizer, ds_path)
     train_dataset = data_module["train_dataset"]
     data_collator = data_module["data_collator"]
 
-    train_dataloader = DataLoader(train_dataset, batch_size=MAX_BATCH_SIZE, shuffle=False, collate_fn=data_collator)
+    train_dataloader = DataLoader(train_dataset, shuffle=False, collate_fn=data_collator)
 
-    print(train_dataset[0])
     print(train_dataset[0]['input_ids'].shape, train_dataset[0]['labels'].shape)
-    #print(Tokenizer.decode(train_dataset[0]['input_ids']))
+    #print(Tokenizer.decode(train_dataset[0]['input_ids'].tolist()))
 
     optimizer = torch.optim.Adam(model.parameters())
     loss_fn = nn.CrossEntropyLoss()
@@ -214,44 +199,34 @@ def train(ds_path, tokenizer_path, ckpt_path):
         losses = list()
         #torch.set_grad_enabled(True)
         for i, data in enumerate(train_dataloader):
-            #inputs_tensor = torch.tensor(data['inputs'], dtype=torch.long)
-            #labels_tensor = torch.tensor(data['target'], dtype=torch.long)
-            #print(inputs_tensor.shape)
-            inputs, labels = data['input_ids'], data['labels']
-            inputs, labels = inputs[:, :-1], labels[:, 1:]
-            print(inputs.shape, labels.shape)
-
-            #with torch.autocast(device_type='cuda', dtype=torch.float16):
-                #outputs = model.forward(inputs, 0)
-                #outputs = model(inputs, 0)
-            #outputs = model(inputs_tensor, 0)
-            outputs = model.forward(inputs, 0)
-            print(outputs.shape)
-
-                #loss = loss_fn(outputs.transpose(1,2), labels)
-            #loss = loss_fn(outputs.transpose(1,2), labels_tensor)
-            loss = loss_fn(outputs.transpose(1,2), labels)
-            #print(labels)
-                #_, loss = model.generate(inputs, MAX_SEQ_LEN, logprobs=True)
-                #loss.backward()
-            loss.backward()
-            #scaler.scale(loss).backward()
-
-            #if i % ACC_STEP == 0:
-
-            #    optimizer.step()
-            optimizer.step()
-            #scaler.step(optimizer)
-
-            #    optimizer.zero_grad()
             optimizer.zero_grad()
-            #scaler.update()
+            inputs, labels = data['input_ids'].to(device), data['labels'].to(device)
+
+            outputs = model(inputs, 0)
+
+            shift_logits = outputs[...,:-1,:].contiguous()
+            shift_labels = labels[..., 1:].contiguous()
+
+            shift_logits = shift_logits.view(-1, 32000)
+            shift_labels = shift_labels.view(-1)
+
+            shift_labels = shift_labels.to(shift_logits.device)
+
+            loss = loss_fn(shift_logits, shift_labels)
+
+            loss.requires_grad=True
+            loss.retain_grad()
+
+            loss.backward()
+
+            optimizer.step()
 
             losses.append(loss.item())
             print("Loss per batch: {}".format(loss.item()))
 
         epoch_loss = np.average(losses)
         loss_per_epoch.append(epoch_loss)
+        print("=======================================")
         print("Epoch: {0}, Epoch Loss: {1}".format(epoch+1, epoch_loss))
 
 
